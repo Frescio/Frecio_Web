385d3f9942559163971559859c5f1008748011b6
Amisha <gargamisha01@gmail.com>
2021-11-21 22:34:19 +0530

    added to wishlist functionality

--- project/ecommerce/views.py
+++ project/ecommerce/views.py
@@ -4,62 +4,71 @@ import csv
 
 from user.models import crop
 from user.models import wishlist
+from django.contrib import messages
 # Create your views here.
 from django.core import serializers
 
 
 def mycrops(request):
     if request.user.is_authenticated:
-        if request.POST:
-            print("bbbbbbb")
-            # name = request.POST.get('name')
-            if request.POST.get('submit') == "add_new_crop":
-                crop_name = request.POST['crop_name']
-                price = request.POST['price']
-                quantity = request.POST['quantity']
-                photo = request.FILES['photo']
-                new_crop = crop( user=request.user, crop_name = crop_name, price=price, quantity=quantity, photo=photo)
-                new_crop.save()
-                print(new_crop)
-            # elif request.POST.get('submit') == "edit_crop":
+        if request.user.isFarmer:
+            if request.POST:
+                print("bbbbbbb")
+                # name = request.POST.get('name')
+                if request.POST.get('submit') == "add_new_crop":
+                    crop_name = request.POST['crop_name']
+                    price = request.POST['price']
+                    quantity = request.POST['quantity']
+                    photo = request.FILES['photo']
+                    new_crop = crop( user=request.user, crop_name = crop_name, price=price, quantity=quantity, photo=photo)
+                    new_crop.save()
+                    print(new_crop)
+                # elif request.POST.get('submit') == "edit_crop":
 
-            #     return 
-            elif request.POST.get('submit') == "delete_crop":
-                print("asdfsad")
-                crop.objects.filter(id=request.POST.get('crop_id')).delete()
+                #     return 
+                elif request.POST.get('submit') == "delete_crop":
+                    print("asdfsad")
+                    crop.objects.filter(id=request.POST.get('crop_id')).delete()
 
-            elif request.POST.get('submit') == "edit_crop":
-                
-                crop_name = request.POST['crop_name']
-                price = request.POST['price']
-                quantity = request.POST['quantity']
-                photo = request.FILES['photo']
-                crop.objects.filter(id=request.POST.get('crop_id')).update(crop_name=crop_name,price=price,quantity=quantity,photo=photo)
-                
-                # return 
-        context = {}
-        # context['crop'] = {'sdf','asd','lol'}
-        data=[]
-        # data = pd.read_csv("fertilizer.csv")
-        with open('home/fertilizer.csv', 'r') as f:
-            reader = csv.reader(f)
-            for row in reader:
-                data.append(row[1])
-        data.sort()
-        context['crop'] = data
+                elif request.POST.get('submit') == "edit_crop":
+                    
+                    crop_name = request.POST['crop_name']
+                    price = request.POST['price']
+                    quantity = request.POST['quantity']
+                    photo = request.FILES['photo']
+                    crop.objects.filter(id=request.POST.get('crop_id')).update(crop_name=crop_name,price=price,quantity=quantity,photo=photo)
+                    
+                    # return 
+            context = {}
+            # context['crop'] = {'sdf','asd','lol'}
+            data=[]
+            # data = pd.read_csv("fertilizer.csv")
+            with open('home/fertilizer.csv', 'r') as f:
+                reader = csv.reader(f)
+                for row in reader:
+                    data.append(row[1])
+            data.sort()
+            context['crop'] = data
 
-        crops_added = crop.objects.filter(user=request.user)
+            crops_added = crop.objects.filter(user=request.user)
+            
+            return render( request, 'ecommerce/view_your_added_crops.html', {'crop':data,'crops_added': crops_added })
         
-        return render( request, 'ecommerce/view_your_added_crops.html', {'crop':data,'crops_added': crops_added })
+        else:
+            messages.info(request, 'Please Register as a Farmer to view this page.')
+            return render( request, 'ecommerce/error_msg.html')
+            # return HttpResponse("Please Register as a Farmer to upload your crops")
     else:
-        return HttpResponse("sorry")
+        messages.info(request, 'Please Login/Register as a Farmer to view this page.')
+        return render( request, 'ecommerce/error_msg.html')
     
 
 def viewcrops(request):
     # return( request, 'ecommerce/view_your_added_crops.html')
     
     if request.user.is_authenticated:
-        all_crops = crop.objects.raw("select * from user_crop where user_crop.id != user_wishlist.crop")
+        all_crops = crop.objects.filter()
+        # all_crops = crop.objects.raw("select * from user_crop where user_crop.id != user_wishlist.crop")
         # print("xxxxxxxxxxxxxxxxxx",all_crops2,all_crops)
         if request.POST:
             crop_id = request.POST['crop_id']
@@ -67,12 +76,18 @@ def viewcrops(request):
             wished.save()
         # for p in crop.objects.raw('SELECT * FROM myapp_crop'):
         #     print(p)
-        wish_list = serializers.serialize("json", wishlist.objects.filter(user=request.user))
+        wish_list = wishlist.objects.filter(user=request.user)
+        # wish_list = serializers.serialize("json", wishlist.objects.filter(user=request.user))
         print(wish_list)
+        for wish in wish_list:
+            # print(wish)
+            # cropp = crop.objects.get()
+            # print(wish.crop.id)
+            all_crops = all_crops.exclude(pk=wish.crop.id)
         # for pair in wish_list:
         #     if pair.crop in all_crops:
 
-        return render( request, 'ecommerce/view_all_crops.html' , { 'all_crops':all_crops } )
+        return render( request, 'ecommerce/view_all_crops.html' , { 'all_crops':all_crops, 'wished_crop':wish_list} )
     else:
         return HttpResponse("Sorry")
 
