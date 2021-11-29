f8bf9982dd4b09d41c7714151ae1317a8c20d3fb
nk183 <nk.er13.6@gmail.com>
2021-11-29 13:39:34 +0530

    integration disease

--- project/home/urls.py
+++ project/home/urls.py
@@ -9,13 +9,13 @@ urlpatterns = [
     path( 'pest_result', views.pest_pre_result, name='pest_res'),
     path( 'fertilizer', views.fertilizer, name='fert_pred'),
     path( 'fert_result', views.fertpred, name='fert_res'),
-    path( 'dis_res', views.dis_res, name="dis_res" ),
     # path('register', views.register, name='register')
     # path('register', views.register, name='register')
     # path('register', include('accounts.urls')),
     path( 'about', views.about, name='aboutus'),
     path( 'contact', views.contact, name='contactus'),
     path( 'dis_pred', views.disease_pred, name="dis_pred"),
+    path( 'disease_result', views.disease_result, name='dis_res'),
     path('profile', views.profile, name='my_profile'),
     path( 'editprofile', views.edit_profile, name = 'edit_profile'),
     path('chatbot', views.chatbot, name='chatbot'),
